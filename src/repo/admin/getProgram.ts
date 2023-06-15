4b3ae088cf6f163b444a1fccdbd757738d4848a6
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2023-06-15 13:01:41 +0900

    fix

--- src/repo/admin/getProgram.ts
+++ src/repo/admin/getProgram.ts
@@ -1,8 +1,5 @@
-import axios from "axios"
-
 import { DATABASE_ID, collections } from "@/lib/appwrite/constants"
-
-import { ProgramSchema } from "../type"
+import { adminDatabase } from "@/lib/appwriteAdmin/core"
 
 const APPWRITE_PRJECT_ID = process.env["NEXT_PUBLIC_APPWRITE_PRJECT_ID"]
 const APP_WRITE_API_KEY = process.env["APP_WRITE_API_KEY"]
@@ -16,42 +13,33 @@ if (APP_WRITE_API_KEY == null) {
 }
 
 export const getProgram = async (id: string) => {
-  console.log(
-    `https://cloud.appwrite.io/v1/databases/${DATABASE_ID}/collections/${collections.programEditor}/documents/${id}`,
-  )
-  const res = await axios.get(
-    `https://cloud.appwrite.io/v1/databases/${DATABASE_ID}/collections/${collections.programEditor}/documents/${id}`,
-    {
-      headers: {
-        "Content-Type": "application/json",
-        "X-Appwrite-Response-Format": "1.0.0",
-        "X-Appwrite-Project": APPWRITE_PRJECT_ID,
-        "X-Appwrite-Key": APP_WRITE_API_KEY,
-      },
-      timeout: 10000,
-    },
-  )
+  // console.log(
+  //   `https://cloud.appwrite.io/v1/databases/${DATABASE_ID}/collections/${collections.programEditor}/documents/${id}`,
+  // )
+  // const res = await axios.get(
+  //   `https://cloud.appwrite.io/v1/databases/${DATABASE_ID}/collections/${collections.programEditor}/documents/${id}`,
+  //   {
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "X-Appwrite-Response-Format": "1.0.0",
+  //       "X-Appwrite-Project": APPWRITE_PRJECT_ID,
+  //       "X-Appwrite-Key": APP_WRITE_API_KEY,
+  //     },
+  //     timeout: 10000,
+  //   },
+  // )
   // const json = await res.json()
 
-  // const doc = await adminDatabase.getDocument(
-  //   DATABASE_ID,
-  //   collections.programEditor,
-  //   id,
-  // )
-  const doc = res.data
+  const doc = await adminDatabase.getDocument(
+    DATABASE_ID,
+    collections.programEditor,
+    id,
+  )
+  // const doc = res.data
   const _doc = doc as any
 
   return {
     id: doc.$id,
-    title: "Unknown",
-    createdBy: {
-      id: _doc["created_by"] as string,
-      name: _doc["created_by_name"] as string,
-    },
-    description: _doc["description"] as string,
-    instruction: _doc["instruction"] as string,
-    isPublic: _doc["is_public"] as boolean,
-    createdAt: doc.$createdAt,
-    updatedAt: doc.$updatedAt,
-  } satisfies ProgramSchema
+    program: _doc["program"] as string,
+  }
 }
